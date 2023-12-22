94e8f6fd99c0509b66d39843f96ad3c5265e2295
OMOTEC <151024042+OMOTEC-ELECTRONICS@users.noreply.github.com>
2023-12-22 13:52:47 +0530

    Activity_02-Notifier.py

--- CHAPTER-01 BASIC INTRACTION/Activity_02-Notifier.py
+++ CHAPTER-01 BASIC INTRACTION/Activity_02-Notifier.py
@@ -8,11 +8,11 @@ while True:
     # Print the current state of the button (0: pressed, 1: not pressed)
     print(Button.value())
  # Check if the button is pressed
-    if Button.value() == 0:
+    if Button.value() == 1:
         # Print a message and wait for 1 second to avoid rapid triggering
         print("Patient Assistance Requested: Room 203")
-        utime.sleep(1)
+        utime.sleep(0.5)
     else:
         # Print a placeholder message and wait for 1 second
         print("--")
-        utime.sleep(1)
+        utime.sleep(0.5)
